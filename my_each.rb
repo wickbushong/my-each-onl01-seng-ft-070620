f859fdb5acb45b4b5922e1679ce851d68d6b552a
Wick Bushong <wickbushong@gmail.com>
2020-07-07 19:48:20 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
 def my_each # put argument(s) here
-  # code here
+  if block_given?
+  i = 0
+
+    while i < array.length
+      yield array[i]
+      i = i + 1
+    end
+  else
+    puts "Hey! No block was given!"
+  end
+  array
 end
